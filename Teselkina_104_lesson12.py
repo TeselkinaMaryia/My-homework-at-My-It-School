ae1e5bf9321cd58fed1e6135b01c00f074441617
Maryia <mshts00@mail.ru>
2022-10-15 12:38:38 +0300

    #4 completed

--- Teselkina_104_lesson12.py
+++ Teselkina_104_lesson12.py
@@ -2,21 +2,21 @@ from typing import Dict
 
 
 # 4
-def function(seconds):
-    def days():
-        return seconds // (24 * 60 * 60)
+def function(seconds: int):  # объявляем функцию с олним параметром
+    def days() -> int:  # объявляем функцию без параметра которая буде возвращать число дней
+        return seconds // (24 * 60 * 60)  # функция, которая возвращает число
 
-    def hours():
-        return (seconds - days() * (24 * 60 * 60)) // (60 * 60)
+    def hours() -> int: # объявляем функцию без параметра которая буде возвращать количество часов
+        return (seconds - days() * (24 * 60 * 60)) // (60 * 60)  # функция, которая возвращает число
 
-    def minuts():
-        return (seconds - days() * (24 * 60 * 60) - hours() * (60 * 60)) // 60
+    def minuts() -> int:  # объявляем функцию без параметра которая буде возвращать количество минут
+        return (seconds - days() * (24 * 60 * 60) - hours() * (60 * 60)) // 60  # функция, которая возвращает число
 
-    def seconds_():
+    def seconds_() -> int:  # объявляем функцию без параметра которая буде возвращать количество секунд
         return (seconds - days() * (24 * 60 * 60) - hours() * (60 * 60) - minuts() * 60) % 60
 
     print(f'{days()} дней, {hours()} часов, {minuts()} минут,{seconds_()} секунд')
 
 
-seconds_ = int(input('Введите число секунд: '))
-function(seconds_)
+seconds_ = int(input('Введите число секунд: '))  # переменная, которая ссылается на введенное с клавиатуры значение
+function(seconds_)  # вызываем функцию
