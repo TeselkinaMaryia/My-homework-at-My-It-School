48d9ae2ce882bb7fa872154c02da12c79d1b6a63
Maryia <mshts00@mail.ru>
2022-10-29 13:35:45 +0300

    end variant

--- lesson_15/Teselkina_104_lesson15.py
+++ lesson_15/Teselkina_104_lesson15.py
@@ -3,24 +3,40 @@ from typing import Dict, List
 
 
 # 1
-def write_in_json(string: str) -> None:  # объявляем функцию с одним параметром
-    list_day_product = []
-    dict_: Dict[str, str] = {}  # переменная, которая ссылается на пустой словарь
-    list_: List[str] = string.split(' ')  # переменная, которая ссылается на список из строк
-    dict_['название'] = list_[0]  # создаем пару ключ: значение
-    dict_['стоимость'] = list_[1]
+def dict_list(string: str) -> None:  # объявляем функцию с одним параметром
+    list_day_product: List = []  # переменная, которая ссылаетяс на пустой список
+    while string != 'стоп':  # цикл с условием
+        dict_: Dict[str, str] = {}  # переменная которая ссылается на пустой словарь
+        list_: List[str] = string.split(' ')  # из строки делвем список
+        dict_['название'] = list_[0]  # создаем пары ключ: значение
+        dict_['стоимость'] = list_[1]
+        list_day_product.append(dict_)  # добавляем словарь в список
+        string: str = input('Введите новый товар и его цену: ')
+        # переменная, которая ссылается на вводимое с клавиатуры значение
 
-    list_day_product.append(dict_)
-    string_1: str = input('Введите название и стоимость продукта через пробел/стоп: ')
-    if string_1 != 'стоп':
-        write_in_json(string_1)
+    if string == 'стоп':  # условие
+        write_json(list_day_product)  # вызываем функцию
 
-    with open('DZ_1.json', 'a', encoding='UTF-8') as file:  # открываем файл
-        json.dump(list_day_product, file, ensure_ascii=False)  # записываем объект пайтона в файл
+
+def write_json(list_: List[dict]) -> None:  # объявляем функцию с одним параметром
+    with open('DZ_1.json', 'w', encoding='UTF-8') as file:  # открываем файл для записи
+        json.dump(list_, file, ensure_ascii=False)  # записываем в файл объект
+
+
+# 2
+def read_json():  # объявляем функцию без параметра
+    with open('DZ_1.json', encoding='UTF-8') as file:  # открываем файл для чтения
+        py: List[dict] = json.load(file)  # записываем в переменную преобразованный в объект пайтона объект json
+        price: int = 0  # переменная, которая ссылается на целое число
+        for i in py:  # цикл
+            price += int(i['стоимость'])  # добавляем к значению переменной стоимость продукта
+        print(f'потрачено денег за день - {price} рублей')  # вывод в кансоль
 
 
 # 1
 string_: str = input('Введите название и стоимость продукта через пробел/стоп: ')
 # переменная, которая ссылается на вводимое с клавиатуры значение
-write_in_json(string_)  # вызываем функцию
+dict_list(string_)  # вызываем функцию
 
+# 2
+read_json()  # вызываем функцию 
