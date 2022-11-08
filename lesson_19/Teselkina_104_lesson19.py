e8f9fdae545028d27a199ebf16f4242dc632a3a4
Maryia <mshts00@mail.ru>
2022-11-08 21:13:25 +0300

    added last task

--- lesson_19/Teselkina_104_lesson19.py
+++ lesson_19/Teselkina_104_lesson19.py
@@ -1,4 +1,5 @@
 from typing import Any, List
+import logging
 
 
 # 1 classwork
@@ -15,7 +16,7 @@ def decorator_1(func: Any) -> Any:  # decorator, accepts function and return bod
 
 
 @decorator_1  # decorating the function
-def sum_(a: int, b: int):  # function accepts two options
+def sum_(a: int, b: int) -> int:  # function accepts two options
     return a + b  # return the sum of the two digits
 
 
@@ -33,7 +34,7 @@ def decorator_2(func):  # decorator accepts function
     return wrapper  # return the body of the function wrapper
 
 
-@decorator_2
+@decorator_2  # decorating the function
 def multiples(list_: list) -> tuple:  # function accept list and return tuple
     new_list = []  # variable that refers to an empty list
     for i in list_:  # cycle
@@ -46,12 +47,46 @@ def multiples(list_: list) -> tuple:  # function accept list and return tuple
     return new_list, multiples_of_three  # return tuple
 
 
+# 3
+def debug(func: Any) -> Any:  # decorator accepts the function
+
+    def wrapper(*args: Any) -> Any:  # function accepts many positional options
+        print('Start work')  # outputting the information to the console
+        res = func(*args)  # variable refers to the function work result
+        if res is not None:  # condition
+            print(f'Function - {func.__name__} - with arguments {args}. Result - {res}')
+            # built-in function output string to the console
+            return res  # return variable value
+
+    return wrapper  # return the body of the function
+
+
+@debug  # decoration the function
+def division(num_1: int, num_2: int) -> float or None:  # function accepts two positional options
+    try:  # block of the code where exceptions can be thrown
+        end_difference = num_1 / num_2
+        return end_difference  # return variable value
+    except Exception:  # block of code that handles exceptions
+        logging.exception('Exception occurred')  # output to the console string and the information of exception
+        print()
+
+        num_1_ = int(input('enter new divident: '))
+        num_2_ = int(input('enter new divider: '))
+        division(num_1_, num_2_)  # call the function and transfer two positional arguments
+
+
 # 1 classwork
-# sum_(1, 3)  # call the function and transfer two positional arguments
-# sum_(4, 5)  # call the function and transfer two positional arguments
-# sum_(b=3, a=6)  # call the function and transfer two keyword arguments
+sum_(1, 3)  # call the function and transfer two positional arguments
+sum_(4, 5)  # call the function and transfer two positional arguments
+sum_(b=3, a=6)  # call the function and transfer two keyword arguments
 
 
 # 2
 list_1: List[Any] = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]  # variable, type = list
 print(multiples(list_1))  # built-in function, output the result of function work to console
+
+
+# 3
+num_1: int = int(input('enter the divident: '))  # variable refers to the number entered from the keyboard
+num_2: int = int(input('enter the divider: '))
+division(num_1, num_2)  # call the function and transfer two positional arguments
